07e603e7f35ddf32e8dae8ea63816c37195530e0
MikeDs87 <micheledesimone46@gmail.com>
2016-02-06 10:57:59 +0000

    final part

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-// console.log('it works');
+
+// function who control the selector button in portfolio
+/*$(document).ready(function() {
+
+	jQuery(':button').click(function () {
+
+	    if ($(this).hasClass("all-button")) {
+
+	       if ($('a').hasClass("print") || $('a').hasClass("web"))  {
+	        	$('a')
+	        	.removeClass("deactive")
+	        	.click(function() { return false; });
+	        };
+
+	    } else if ($(this).hasClass("print-button")) {
+	    	if ($('a').hasClass("web"))  {
+	        	
+	        	$('a.web').addClass(" deactive")
+	        	$('a.web').removeAttr("href");
+	        	$('a.print').removeClass("deactive");
+	        };
+	
+	    } else if ($(this).hasClass("web-button")) {
+	    	 	if ($('a').hasClass("print"))  {
+	        	$('a.print')
+	        	.addClass(" deactive")
+	        	.click(function() { return false; });
+	        	$('a.web').removeClass("deactive");
+	        };
+
+	        
+	    }
+	});
+
+});*/
